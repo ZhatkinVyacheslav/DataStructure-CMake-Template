7fd492d1363dda54bac4dd32d7c5d1d5c7f72ea8
ZhatkinVyacheslav <90867068+ZhatkinVyacheslav@users.noreply.github.com>
2023-04-11 01:14:40 +0300

    Доделал дерево(не факт что работает)

--- lib_tree/tree.h
+++ lib_tree/tree.h
@@ -1,5 +1,13 @@
 #pragma once
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../lib_StackFromList/StackFromList.h"
+
+template <class Type>
+class Tree;
+template <class Type>
+class FindTree;
 
 template <class Type>
 class Cnode {
@@ -9,6 +17,7 @@ class Cnode {
 
 public:
 
+	friend class FindTree<Type>;
 
 	Cnode(Type data1) {
 		data = data1;
@@ -20,24 +29,12 @@ public:
 		Right = nullptr;
 	}
 
-	void SetData(Type data1) {
+	Cnode(Type data1, Cnode<Type>* Left1, Cnode<Type>* Right1)
+	{
 		data = data1;
+		Left = Left1;
+		Right = Right1; 
 	}
-
-	void SetLeft(Cnode<Type>* newNode) {
-		Left = newNode;
-	}
-
-	void SetRight(Cnode<Type>* newNode) {
-		Right = newNode;
-	}
-
-	Type GetData() {
-		return data;
-	}
-
-	Cnode<Type>* GetLeft() { return Left; }
-	Cnode<Type>* GetRight() { return Right; }
 
 	Cnode operator=(const Cnode<Type> right) {
 		this->data = right.data;
@@ -96,15 +93,15 @@ public:
 		if (root == NULL) root = NewNode;
 		else {
 			Cnode <Type>* Copy = root;
-			while (Copy->GetLeft() != NewNode && Copy->GetRight() != NewNode) {
-				if (Copy->GetData() < NewNode->GetData())
+			while (Copy->Left != NewNode && Copy->Right != NewNode) {
+				if (Copy->data < NewNode->data)
 				{
-					if (Copy->GetLeft() == nullptr) Copy->SetLeft(NewNode);
-					else Copy = Copy->GetLeft();
+					if (Copy->Left == nullptr) Copy->Left = NewNode;
+					else Copy = Copy->Left;
 				}
 				else {
-					if (Copy->GetRight() == nullptr) Copy->SetRight(NewNode);
-					else Copy = Copy->GetRight();
+					if (Copy->Right == nullptr) Copy->Right = NewNode;
+					else Copy = Copy->Right;
 				}
 			}
 		}
@@ -115,18 +112,18 @@ public:
 		bool flag = true;
 		while (flag)
 		{
-			if (Copy->GetData() == findData) return Copy;
-			if (Copy->GetData() < findData)
+			if (Copy->data == findData) return Copy;
+			if (Copy->data < findData)
 			{
-				if (Copy->GetLeft() == nullptr) flag = false;
-				else Copy = Copy->GetLeft();
+				if (Copy->Left == nullptr) flag = false;
+				else Copy = Copy->Left;
 			}
 			else {
-				if (Copy->GetRight() == nullptr) flag = false;
-				else Copy = Copy->GetRight();
+				if (Copy->Right == nullptr) flag = false;
+				else Copy = Copy->Right;
 			}
 		}
-		if (Copy->GetData() == findData) return Copy;
+		if (Copy->data == findData) return Copy;
 		return nullptr;
 	}
 
@@ -135,45 +132,45 @@ public:
 		bool flag = false;
 		Cnode <Type>* Copy = root;
 		Cnode <Type>* DelNode = root;
-		while (DelNode->GetData() != element )
+		while (DelNode->data != element )
 		{
-			if (Copy->GetLeft()->GetData() == element) DelNode = Copy->GetLeft();
-			else if (Copy->GetRight()->GetData() == element) DelNode = Copy->GetRight();
+			if (Copy->Left->data == element) DelNode = Copy->Left;
+			else if (Copy->Right->data == element) DelNode = Copy->Right;
 				else {
-					if (Copy->GetData() < element) Copy = Copy->GetLeft();
-					else Copy = Copy->GetRight();
+					if (Copy->data < element) Copy = Copy->Left;
+					else Copy = Copy->Right;
 				}
 		}
 		if (DelNode != root)
 		{
-			if (Copy->GetLeft() == DelNode)
+			if (Copy->Left == DelNode)
 			{
-				if (DelNode->GetRight() != nullptr) Copy->SetLeft(DelNode->GetRight());
-				else if (DelNode->GetLeft() != nullptr) Copy->SetLeft(DelNode->GetLeft());
+				if (DelNode->Right != nullptr) Copy->Left = DelNode->Right;
+				else if (DelNode->Left != nullptr) Copy->Left = DelNode->Left;
 				else
 				{
 					flag = true;
-					Copy->SetLeft(nullptr);
+					Copy->Left = nullptr;
 				}
 			}
 			else {
-				if (DelNode->GetRight() != nullptr) Copy->SetRight(DelNode->GetRight());
-				else if (DelNode->GetLeft() != nullptr) Copy->SetRight(DelNode->GetLeft());
+				if (DelNode->Right != nullptr) Copy->Right = DelNode->Right;
+				else if (DelNode->Left != nullptr) Copy->Right = DelNode->Left;
 				else
 				{
 					flag = true;
-					Copy->SetRight(nullptr);
+					Copy->Right = nullptr;
 				}
 			}
 		}
-		else if (DelNode->GetRight() != nullptr) {
-			root = DelNode->GetRight();
-			DelNode = DelNode->GetLeft();
+		else if (DelNode->Right != nullptr) {
+			root = DelNode->Right;
+			DelNode = DelNode->Left;
 		}
-		else if (DelNode->GetLeft() != nullptr)
+		else if (DelNode->Left != nullptr)
 		{
-			root = DelNode->GetRight();
-			DelNode = DelNode->GetLeft();
+			root = DelNode->Right;
+			DelNode = DelNode->Left;
 		}
 		else {
 			root = nullptr;
@@ -183,18 +180,74 @@ public:
 		if (!flag)
 		{
 			Copy = root;
-			while (Copy->GetLeft() != DelNode && Copy->GetRight() != DelNode) {
-				if (Copy->GetData() < DelNode->GetData())
+			while (Copy->Left != DelNode && Copy->Right != DelNode) {
+				if (Copy->data < DelNode->data)
 				{
-					if (Copy->GetLeft() == nullptr) Copy->SetLeft(DelNode);
-					else Copy = Copy->GetLeft();
+					if (Copy->Left == nullptr) Copy->Left = DelNode;
+					else Copy = Copy->Left;
 				}
 				else {
-					if (Copy->GetRight() == nullptr) Copy->SetRight(DelNode);
-					else Copy = Copy->GetRight();
+					if (Copy->Right == nullptr) Copy->Right = DelNode;
+					else Copy = Copy->Right;
 				}
 			}
 		}
+	}
+
+	void print() {
+		/*PrintTree(root, 1);*/
+		PrintTree(root);
+	}
+
+private:
+	void PrintTree(Cnode<Type>* curr) {
+		if (curr) // Проверка на ненулевой указатель
+		{
+			PrintTree(curr->Left);
+			cout << curr->data << " ";
+			PrintTree(curr->Right);
+		}
+		/*if (curr != nullptr)
+		{
+			if (lenghtEl(curr->data) == level) cout << curr->data << " ";
+			else if (lenghtEl(curr->data) < level) {
+				PrintTree(curr->Left, level);
+				PrintTree(curr->Right, level);
+			}
+		}
+		level++;
+		cout << "\n";
+		PrintTree(root, level);*/
+	}
+
+
+	int lenghtEl(Type findData) {
+		Cnode <Type>* Copy = root;
+		int res = 1;
+		bool flag = true;
+		while (flag)
+		{
+			if (Copy->data == findData) return res;
+			if (Copy->data < findData)
+			{
+				if (Copy->Left == nullptr) flag = false;
+				else
+				{
+					Copy = Copy->Left;
+					res++;
+				}
+			}
+			else {
+				if (Copy->Right == nullptr) flag = false;
+				else
+				{
+					Copy = Copy->Right;
+					res++;
+				}
+			}
+		}
+		if (Copy->data == findData) return res;
+		return 0;
 	}
 };
 
@@ -203,23 +256,110 @@ public:
 template <class Type>
 class MathTree : public Tree <Type>
 {
+private:
 	Cnode<char>* root;
+
+	void postorder(Node* root1) {
+		if (root == nullptr) {
+			return;
+		}
+
+		postorder(root1->left);
+		postorder(root1->right);
+		cout << root1->data;
+	}
+
+	void inorder(Node * root1) {
+		if (root1 == nullptr) {
+			return;
+		}
+
+		if (isOperator(root1->data)) {
+			cout << "(";
+		}
+
+		inorder(root1->left);
+		cout << root1->data;
+		inorder(root1->right);
+
+		if (isOperator(root1->data)) {
+			cout << ")";
+		}
+	}
+
 public:
-	MathTree(std::string expression) {
-
+	MathTree() {
+		root = nullptr;
 	}
 
-	void DelElement(Type element) override {
+	void Push(string postfix) {
 
+		if (postfix.length() == 0) {
+			return nullptr;
+		}
+
+		StackFromList<Cnode<char>*> s;
+
+		for (char c : postfix)
+		{
+			if (isOperator(c)) {
+				Cnode<char>* x = s.top();
+				s.Pop();
+				Cnode<char>* y = s.top();
+				s.Pop();
+				Cnode<char>* node = new Cnode<char>*(c, y, x);
+				s.Push(node);
+			} else s.Push(new Cnode<char>*(c));
+		}
+		root = s.Top();
 	}
 
-	void Push(Type newElement) override {
-
+	bool isOperator(char c) {
+		return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
 	}
 
-	float Calculate() {
-		float res;
+	void PostPrint() {
+		postorder(root);
+	}
 
+	void InfPrint() {
+		inorder(root);
+	}
+
+	char Calculate(Cnode<char>* root1) {
+		int res;
+		if (isOperator(root1->Left->data)) {
+			Cnode<char>* NewLeftNode = new Cnode<char>*(Calculate(root1->Left));
+			root1->Left = NewLeftNode;
+		}
+		if (isOperator(root1->Right->data)) {
+			Cnode<char>* NewRightNode = new Cnode<char>*(Calculate(root1->Right));
+			root1->Right = NewRightNode;
+		}
+
+		switch (root1->data)
+		{
+		case '+': {
+			res = atoi(root1->Left->data) + atoi(root1->Right->data);
+			break;
+		}
+		case '-': {
+			res = atoi(root1->Left->data) - atoi(root1->Right->data);
+			break;
+		}
+		case '*': {
+			res = atoi(root1->Left->data) * atoi(root1->Right->data);
+			break;
+		}
+		case '/': {
+			res = atoi(root1->Left->data) / atoi(root1->Right->data);
+			break;
+		}
+			default:
+				break;
+		}
+
+		char result = (char)(((int)'0') + res);
 		return result;
 	}
 };
