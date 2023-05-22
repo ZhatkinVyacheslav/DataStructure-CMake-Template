f1e80750d2d3cdac9e8ddc09f6c8d67695bea300
ZhatkinVyacheslav <90867068+ZhatkinVyacheslav@users.noreply.github.com>
2023-05-22 19:11:21 +0300

    мейн, в котором создаётся лабиринт

--- main/main.cpp
+++ main/main.cpp
@@ -5,9 +5,18 @@
 #include "HashTable.h"
 #include <iostream>
 #include <tree.h>
+#include <DSU.h>
 
 void main() {
-	MathTree MT;
-	MT.Push("23-1/");
-	std::cout << MT.Calculate();
+	MazeDSU MD(25);
+	MD.AddWall(5, 6);
+	MD.AddWall(6, 7);
+	MD.AddWall(3, 8);
+	MD.AddWall(11, 16);
+	MD.AddWall(16, 21);
+	MD.AddWall(16, 17);
+	MD.AddWall(17, 18);
+	MD.AddWall(12, 13);
+	MD.AddWall(13, 14);
+	MD.print();
 }
